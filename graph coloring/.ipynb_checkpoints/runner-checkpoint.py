b956fb484dc7566e74158b310a678d130d05c8e6
Mahmud Allahverdiyev <tr0j4n034@gmail.com>
2018-12-16 13:00:37 +0300

    visualization fixes

--- graph coloring/.ipynb_checkpoints/runner-checkpoint.py
+++ graph coloring/.ipynb_checkpoints/runner-checkpoint.py
@@ -22,3 +22,21 @@ def prepare(file):
         value = int(line.split()[-1])
         colors.append(value)
     return colors
+
+def get_max_clique(file):
+    #print("python main function")
+    cmd = "max_clique"
+    #file_name = str(sys.argv).split()[1][1:-2]
+    file_name = file
+    call(["make", cmd]) # compilation
+    output = call(["./" + cmd, file_name])
+
+    f = open("clique_output.txt", "r")
+    data = f.read()
+    lines = data.split("\n")
+    return int(lines[0].split()[-1])
+
+def bound_quantum_chromatic_number(file):
+    coloring = prepare(file)
+    clique = get_max_clique(file)
+    return clique, max(coloring) + 1
